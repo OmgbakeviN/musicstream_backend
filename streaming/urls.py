a303b91b9499340f1ab3a2a971e5fcf2d9ae923b
OmgbaKevin <kevinomgba1@gmail.com>
2025-05-11 12:21:37 +0100

    started frontend

--- streaming/urls.py
+++ streaming/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import search_video, add_to_playlist, get_playlist
 
 urlpatterns = [
     path('search/',views.search_video, name='search_video'),
+    path('playlist/add/', add_to_playlist, name='add_to_playlist'),
+    path('playlist/', get_playlist, name='get_playlist'),
+
 ]
--- streaming/views.py
+++ streaming/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 import subprocess
 import json
 # Create your views here.
@@ -40,3 +42,35 @@ def search_video(request):
     except subprocess.CalledProcessError as e:
         print("Erreur subprocess:", e.stderr)
         return JsonResponse({'error': 'yt-dlp a échoué à exécuter la commande.'}, status=500)
+
+
+@csrf_exempt
+@require_POST
+def add_to_playlist(request):
+    try:
+        data = json.loads(request.body)
+        video = {
+            'id': data['id'],
+            'title': data['title'],
+            'url': data['url'],
+            'thumbnail': data.get('thumbnail'),
+            'duration': data.get('duration'),
+        }
+
+        # Initialiser la playlist si elle n’existe pas
+        if 'playlist' not in request.session:
+            request.session['playlist'] = []
+
+        # Ajouter la chanson si moins de 20 chansons
+        if len(request.session['playlist']) < 20:
+            request.session['playlist'].append(video)
+            request.session.modified = True
+            return JsonResponse({'message': 'Ajouté à la playlist'}, status=200)
+        else:
+            return JsonResponse({'error': 'Limite de 20 chansons atteinte'}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+def get_playlist(request):
+    playlist = request.session.get('playlist', [])
+    return JsonResponse(playlist, safe=False)
